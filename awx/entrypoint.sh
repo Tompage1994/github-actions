3346cccca31d70352b34c55b3c843dcc044b8989
Tom Page <tpage@redhat.com>
2020-06-29 17:26:12 +0100

    Do the git clone in entrypoint

--- awx/entrypoint.sh
+++ awx/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git clone https://github.com/ansible/awx.git
 cd awx/installer
 ansible-playbook -i inventory install.yml -e create_preload_data=false
 until $(curl --output /dev/null --silent --head --fail http://localhost/api/v2/ping); do
