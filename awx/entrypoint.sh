78251c87727492f55bccf1b9a1e9d309951e530b
Tom Page <tpage@redhat.com>
2020-06-29 17:20:40 +0100

    install curl and bash and make entrypoint executable

--- awx/entrypoint.sh
+++ awx/entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd awx/installer
 ansible-playbook -i inventory install.yml -e create_preload_data=false
 until $(curl --output /dev/null --silent --head --fail http://localhost/api/v2/ping); do
