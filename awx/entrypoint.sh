ba2d545ae0d4aadf2c70295a077ed94b749587e0
Tom Page <tpage@redhat.com>
2020-06-29 17:08:28 +0100

    transfer install of awx to entrypoint

--- awx/entrypoint.sh
+++ awx/entrypoint.sh
@@ -1,3 +1,5 @@
+cd awx/installer
+ansible-playbook -i inventory install.yml -e create_preload_data=false
 until $(curl --output /dev/null --silent --head --fail http://localhost/api/v2/ping); do
     printf '.'
     sleep 5
