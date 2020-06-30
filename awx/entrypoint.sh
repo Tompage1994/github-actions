75a572d7c845bdd154e2a7b3235feea02d0749d4
Tom Page <tpage@redhat.com>
2020-06-30 09:43:51 +0100

    Add an option for running test script

--- awx/entrypoint.sh
+++ awx/entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+set -ex
+
+# Download awx installer
 git clone https://github.com/ansible/awx.git
-cd awx/installer
-ansible-playbook -i inventory install.yml -e create_preload_data=false
+cd awx
+git checkout $1
+cd installer
+
+# Install AWX
+ansible-playbook -i inventory install.yml -e create_preload_data=false -e docker_compose_dir=/tmp/awxcompose
+
+# Wait for awx to be alive
 until $(curl --output /dev/null --silent --head --fail http://localhost/api/v2/ping); do
     printf '.'
     sleep 5
 done
-/bin/sh
+
+# Run test script
+cd ../..
+./$2
